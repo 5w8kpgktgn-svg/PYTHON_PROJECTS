62f7a8c716d1b88cb73b8ea38f26d1f1017bfb4c
Emirhan Berber <5w8kpgktgn@privaterelay.appleid.com>
2025-12-13 19:29:53 +0300

    password generator.py

--- password generator.py
+++ password generator.py
@@ -7,36 +7,37 @@ def generator_password(
         digits=True,
         symbol=True
 ):
-    upper_letters=string.ascii_uppercase
-    lower_letters=string.ascii_lowercase
-    digits=string.digits
-    symbol="!@#$%^&*()-_=+[]{};:,.<>?/"
+    upper_list=string.ascii_uppercase
+    lower_list=string.ascii_lowercase
+    digits_list=string.digits
+    symbol_list="!@#$%^&*()-_=+[]{};:,.<>?/"
 
     caracter=""
     password=[]
 
     if upper:
-        caracter+=upper_letters
-        password.append(secrets.choice(upper_letters))
+        caracter+=upper_list
+        password.append(secrets.choice(upper_list))
     if lower:
-        caracter+=lower_letters
-        password.append(secrets.choice(lower_letters))
+        caracter+=lower_list
+        password.append(secrets.choice(lower_list))
     if digits:
-        caracter+=digits
-        password.append(secrets.choice(digits))
+        caracter+=digits_list
+        password.append(secrets.choice(digits_list))
     if symbol:
-        caracter+=symbol
-        password.append(secrets.choice(symbol))
+        caracter+=symbol_list
+        password.append(secrets.choice(symbol_list))
     if not caracter:
         raise ValueError("at least one character type must be selected!")
     if length<len(password):
         raise ValueError("The password length cannot be less than the selected character type!")
     
-    remaining_length=length-len(password)
-    for _ in range(remaining_length):
+    minus=length-len(password)
+    for _ in range(minus):
         password.append(secrets.choice(caracter))
     
     secrets.SystemRandom().shuffle(password)
     return "".join(password)
 password=generator_password(length=8)
-print("generated password",password)
+print("generated password:",password)
+
